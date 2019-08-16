344ba8b0f43de25c8d076ba16d354eba085d10d1
LevAriC <levaricohen@gmail.com>
2019-08-16 20:32:45 +0300

    Finished Assignment
    Baruch Ashem

---
+++ Assets/Scripts/SC_Cube.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Cube : MonoBehaviour
+{
+    public float speed;
+    public Material red, blue;
+    private bool isGoingUp = true;
+    private bool isMoving = true;
+    private bool isRed = false;
+    private Renderer cubeColor;
+
+    private void Awake()
+    {
+        cubeColor = GetComponent<MeshRenderer>();
+        ChangeCubeColor();
+    }
+
+    void OnMouseDown()
+    {
+        ChangeCubeState();
+    }
+
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            if (isGoingUp)
+            {
+                if (transform.position.y >= 10f)
+                    isGoingUp = false;
+                else
+                    transform.Translate(0, speed, 0);
+            }
+            else
+            {
+                if (transform.position.y <= -10f)
+                    isGoingUp = true;
+                else
+                    transform.Translate(0, -speed, 0);
+            }
+        }
+    }
+
+    public void ChangeCubeState()
+    {
+        if(isMoving)
+            isMoving = false;
+        else
+            isMoving = true;
+    }
+
+    public void ChangeCubeColor()
+    {
+        if (isRed)
+            cubeColor.material = blue;
+        else
+            cubeColor.material = red;
+        isRed = !isRed;
+    }
+}
---
+++ Assets/Scripts/SC_Manager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_Manager : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject light;
+    private GameObject curCube;
+    private SC_Cube cubeScript;
+    private Text timeText;
+    private int lightRotation = 0;
+
+    void Awake()
+    {
+        curCube = (GameObject)Instantiate(cube);
+        cubeScript = curCube.gameObject.GetComponent<SC_Cube>();
+        timeText = GameObject.Find("timeText").GetComponent<Text>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            getTimeSinceBeginning(true);
+        if (Input.GetKeyDown(KeyCode.T))
+            cubeScript.ChangeCubeState();
+        if (Input.GetKeyDown(KeyCode.Q))
+            cubeScript.ChangeCubeColor();
+
+        light.transform.eulerAngles = new Vector3(-20, lightRotation, 0);
+        lightRotation += 3;
+
+        timeText.text = getTimeSinceBeginning().ToString();
+    }
+
+    int getTimeSinceBeginning(bool loop = false)
+    {
+        if(loop)
+        {
+            for(short i = 0; i < 10; i++)
+                Debug.Log("Created Time " + Time.time);
+        }
+
+        return (int)Time.time;
+    }
+}
